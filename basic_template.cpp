da09e4a52db654da0f4f28f84c31147c984e185d
Khan Muhammad Rifat <140003326+khan1203@users.noreply.github.com>
2024-12-13 23:15:18 +0600

    basic bitwise operations added

--- basic_template.cpp
+++ basic_template.cpp
@@ -172,6 +172,32 @@ string decimalToBinayString(ll a)
     return bin;
 }
 
+int check_kth_bit_on_or_off(int n, int k) {
+   return (n >> k) & 1;
+}
+
+void print_on_and_off_bits(int n) {
+   for (int k = 30;k >= 0;k--) {
+      if (check_kth_bit_on_or_off(n, k)) {
+         cout << 1 << " ";
+      }
+      else {
+         cout << 0 << " ";
+      }
+   }
+}
+
+int turn_on_kth_bit(int n, int k) {
+   return (n | (1 << k));
+}
+
+int turn_off_kth_bit(int n, int k) {
+   return (n & (~(1 << k)));
+}
+
+int toggle_kth_bit(int n, int k) {
+   return (n ^ (1 << k));
+}
 
 int main()
 {
